7fa1d20368b7e177e4af3e7ed0f599eac9721823
realemeralds <102229990+realemeralds@users.noreply.github.com>
2022-06-13 23:31:54 +0800

    revert this frfr

--- App.js
+++ App.js
@@ -40,10 +40,6 @@ export default function App() {
     };
   }, []);
 
-  if (isLoading) {
-    return <AppLoading />;
-  }
-
   useEffect(() => {
     setTimeout(() => {
       setIsLoading(false);
